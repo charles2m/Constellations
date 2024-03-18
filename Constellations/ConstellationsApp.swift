854500d02c6b2f25a896b82f667c91d0f239957e
charles2m <ch.mmartiniere@hotmail.com>
2024-03-18 17:25:38 +0100

    Initial Commit

---
+++ Constellations/ConstellationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConstellationsApp.swift
+//  Constellations
+//
+//  Created by Charles Martin Martinière on 18/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ConstellationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
