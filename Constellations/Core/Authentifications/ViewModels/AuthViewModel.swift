52ab8432749ef5eb3fe54f3e2d50748c0102dd7e
charles2m <ch.mmartiniere@hotmail.com>
2024-04-02 21:59:36 +0200

    Version(.0

---
+++ Constellations/Core/Authentifications/ViewModels/AuthViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  AuthViewModel.swift
+//  Constellations
+//
+//  Created by Charles Martin Martinière on 02/04/2024.
+//
+
+import SwiftUI
+
+struct AuthViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthViewModel()
+}
